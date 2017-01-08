b5ec82f06ae4e688b29f7d4ace380090954d8ebb
HugeTerry <xuxt-314@qq.com>
2017-01-08 12:12:52 +0800

    (remove .idea)

--- app/src/main/java/us/xingkong/jueqian/module/MainActivity.java
+++ app/src/main/java/us/xingkong/jueqian/module/MainActivity.java
@@ -1,7 +1,14 @@
-package us.xingkong.jueqian;
+package us.xingkong.jueqian.module;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import us.xingkong.jueqian.R;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 17/1/8 10:50
+ */
 
 public class MainActivity extends AppCompatActivity {
 
