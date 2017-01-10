ebda32455b83c4050c2a1859c271667100640ce8
li853281682 <853281682@qq.com>
2017-01-10 17:36:59 +0800

    注册页123

--- app/src/main/java/us/xingkong/jueqian/module/Regist/RegistContract.java
+++ app/src/main/java/us/xingkong/jueqian/module/Regist/RegistContract.java
@@ -12,6 +12,6 @@ public interface RegistContract {
     }
     interface Presenter extends BasePresenter {
         void regist(final Context context, final String username, final String password);
-        void a();
+        //void a();
     }
 }
