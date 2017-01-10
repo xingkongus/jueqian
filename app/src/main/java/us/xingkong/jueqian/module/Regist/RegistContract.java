b785c18b3792a7c2a24f1cc0c1bf5e2970417842
li853281682 <853281682@qq.com>
2017-01-10 17:29:09 +0800

    注册页

---
+++ app/src/main/java/us/xingkong/jueqian/module/Regist/RegistContract.java
@@ -0,0 +1,18 @@
+package us.xingkong.jueqian.module.Regist;
+
+import android.content.Context;
+
+import us.xingkong.jueqian.base.BasePresenter;
+import us.xingkong.jueqian.base.BaseView;
+
+
+
+public interface RegistContract {
+    interface View extends BaseView<Presenter> {
+    }
+    interface Presenter extends BasePresenter {
+        void regist(final Context context, final String username, final String password);
+        void a();
+        //
+    }
+}
