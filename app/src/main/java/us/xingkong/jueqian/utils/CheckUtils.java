93b6ae3e1ad6f96a4b78cfa0b08807fcaf8d5bd2
HugeTerry <xuxt-314@qq.com>
2017-01-10 16:24:08 +0800

    添加正则工具类

---
+++ app/src/main/java/us/xingkong/jueqian/utils/CheckUtils.java
@@ -0,0 +1,83 @@
+package us.xingkong.jueqian.utils;
+
+import android.text.TextUtils;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 17/1/10 16：23
+ */
+
+public class CheckUtils {
+
+    /**
+     * 正则表达式：验证手机号
+     */
+    private static final String REGEX_MOBILE = "^((13[0-9])|(14[5,7])|(15[^4,\\D])|(17[6-8])|(18[0-9]))\\d{8}$";
+
+    /**
+     * 正则表达式：验证密码
+     */
+    private static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,16}$";
+
+    /**
+     * 正则表达式：验证邮箱
+     */
+    private static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    public static boolean checkPhone(String phone) {
+        return Pattern.matches(REGEX_MOBILE, phone);
+    }
+
+    public static boolean checkPassword(String password) {
+        return Pattern.matches(REGEX_PASSWORD, password);
+    }
+
+    public static boolean checkEmail(String email) {
+        return Pattern.matches(REGEX_EMAIL, email);
+    }
+
+    public static boolean checkVerifyCode(String verifyCode) {
+        return verifyCode.length() == 4;
+    }
+
+    /**
+     * 校验银行卡卡号
+     *
+     * @param cardId
+     * @return
+     */
+    public static boolean checkBankCard(String cardId) {
+        if (TextUtils.isEmpty(cardId)) {
+            return false;
+        }
+        char bit = getBankCardCheckCode(cardId.substring(0, cardId.length() - 1));
+        return bit != 'N' && cardId.charAt(cardId.length() - 1) == bit;
+    }
+
+    /**
+     * 从不含校验位的银行卡卡号采用 Luhm 校验算法获得校验位
+     *
+     * @param nonCheckCodeCardId
+     * @return
+     */
+    private static char getBankCardCheckCode(String nonCheckCodeCardId) {
+        if (nonCheckCodeCardId == null || nonCheckCodeCardId.trim().length() == 0
+                || !nonCheckCodeCardId.matches("\\d+")) {
+            //如果传的不是数据返回N
+            return 'N';
+        }
+        char[] chs = nonCheckCodeCardId.trim().toCharArray();
+        int luhmSum = 0;
+        for (int i = chs.length - 1, j = 0; i >= 0; i--, j++) {
+            int k = chs[i] - '0';
+            if (j % 2 == 0) {
+                k *= 2;
+                k = k / 10 + k % 10;
+            }
+            luhmSum += k;
+        }
+        return (luhmSum % 10 == 0) ? '0' : (char) ((10 - luhmSum % 10) + '0');
+    }
+}
