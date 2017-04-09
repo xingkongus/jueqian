ad1850fc10b62a2c8ea23d1a7981492f7fd842c0
li853281682 <853281682@qq.com>
2017-04-09 14:00:07 +0800

    解决拉下来一大堆错乱merge，添加回答问题

--- app/src/main/java/us/xingkong/jueqian/bean/ForumBean/BombBean/Question.java
+++ app/src/main/java/us/xingkong/jueqian/bean/ForumBean/BombBean/Question.java
@@ -17,84 +17,11 @@ public class Question extends BmobObject {
 
     private _User user;//提问题的用户
     private Integer focus;/*关注的数量*/
-    private BmobRelation answer;
+    private BmobRelation answers;
     private BmobRelation likepeople;
     private BmobRelation collectpeople;/*收藏的人的列表*/
     private BmobRelation focuspeople;/*关注的人的列表*/
-
-    public Integer getFocus() {
-        return focus;
-    }
-
-    public void setFocus(Integer focus) {
-        this.focus = focus;
-    }
-
-    public BmobRelation getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(BmobRelation answer) {
-        this.answer = answer;
-    }
-
-    public BmobRelation getLikepeople() {
-        return likepeople;
-    }
-
-    public void setLikepeople(BmobRelation likepeople) {
-        this.likepeople = likepeople;
-    }
-
-    public BmobRelation getCollectpeople() {
-        return collectpeople;
-    }
-
-    public void setCollectpeople(BmobRelation collectpeople) {
-        this.collectpeople = collectpeople;
-    }
-
-    public BmobRelation getFocuspeople() {
-        return focuspeople;
-    }
-
-    public void setFocuspeople(BmobRelation focuspeople) {
-        this.focuspeople = focuspeople;
-    }
-
-    public _User getUser() {
-        return user;
-    }
-
-    public void setUser(_User user) {
-        this.user = user;
-
-    }
-
-    public String getTAG1_ID() {
-        return TAG1_ID;
-    }
-
-    public void setTAG1_ID(String TAG1_ID) {
-        this.TAG1_ID = TAG1_ID;
-    }
-
-    public String getTAG2_ID() {
-        return TAG2_ID;
-    }
-
-    public String getMcontent() {
-        return mcontent;
-    }
-
-    public Integer getFocus() {
-        return focus;
-    }
-
-    public void setFocus(Integer focus) {
-        this.focus = focus;
-    }
-
+    private Integer state;
 
     public Integer getState() {
         return state;
@@ -102,6 +29,14 @@ public class Question extends BmobObject {
 
     public void setState(Integer state) {
         this.state = state;
+    }
+
+    public String getMtitle() {
+        return mtitle;
+    }
+
+    public void setMtitle(String mtitle) {
+        this.mtitle = mtitle;
     }
 
     public String getTAG1_ID() {
@@ -120,4 +55,59 @@ public class Question extends BmobObject {
         this.TAG2_ID = TAG2_ID;
     }
 
+    public String getMcontent() {
+        return mcontent;
+    }
+
+    public void setMcontent(String mcontent) {
+        this.mcontent = mcontent;
+    }
+
+    public Integer getFocus() {
+        return focus;
+    }
+
+    public void setFocus(Integer focus) {
+        this.focus = focus;
+    }
+
+    public _User getUser() {
+        return user;
+    }
+
+    public void setUser(_User user) {
+        this.user = user;
+    }
+
+    public BmobRelation getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(BmobRelation answers) {
+        this.answers = answers;
+    }
+
+    public BmobRelation getLikepeople() {
+        return likepeople;
+    }
+
+    public void setLikepeople(BmobRelation likepeople) {
+        this.likepeople = likepeople;
+    }
+
+    public BmobRelation getFocuspeople() {
+        return focuspeople;
+    }
+
+    public void setFocuspeople(BmobRelation focuspeople) {
+        this.focuspeople = focuspeople;
+    }
+
+    public BmobRelation getCollectpeople() {
+        return collectpeople;
+    }
+
+    public void setCollectpeople(BmobRelation collectpeople) {
+        this.collectpeople = collectpeople;
+    }
 }
